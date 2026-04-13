67c2919155905cc1a9614e434089e4ff80b3d896
seppe09 <modsahilprof0@gmail.com>
2026-04-14 00:47:14 +0530

    created custom user, teacher & student tables

--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "accounts.User"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -104,3 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
